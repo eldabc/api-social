bceff13926ea15087732816ebcf4e69b4253dd10
eldabc <eldab@htdevs.com>
2021-04-12 23:26:33 -0400

    Added missing endpoints to admin

--- app/Http/Controllers/API/AdminController.php
+++ app/Http/Controllers/API/AdminController.php
@@ -6,6 +6,7 @@ use DB;
 use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use App\Http\Controllers\Controller;
@@ -36,26 +37,22 @@ class AdminController extends Controller
     {
         $user = User::with('roles')->where('id', $id)->first();
 
-        $join = '';
-        $select = '';
         if( $user->roles[0]->id === 2 )//user cliente
         {
-            $query = 
-            Order::join('order_details', 'order_details.order_id', 'orders.id')  
-                    ->join('users', 'users.id', 'order_details.distr_id')
-                    ->join('plans', 'order_details.plan_id', 'plans.id')   
-                    ->join('products', 'plans.product_id', 'products.id')   
-                    ->where('orders.user_id', $id)
-                    ->select(
-                            'products.name as name_product', 'products.presentation',
-                            'order_details.quantity', 'users.name as name_distr', 'orders.created_at as created_at_order','orders.delivery_address as delivery_address_order',
-                            'orders.city as city_order','orders.total_order',
-                            )
-                    ->get();
+            $query =  Order::join('order_details', 'order_details.order_id', 'orders.id')  
+                            ->join('users', 'users.id', 'order_details.distr_id')
+                            ->join('plans', 'order_details.plan_id', 'plans.id')   
+                            ->join('products', 'plans.product_id', 'products.id')   
+                            ->where('orders.user_id', $id)
+                            ->select(
+                                    'products.name as name_product', 'products.presentation',
+                                    'order_details.quantity', 'users.name as name_distr', 'orders.created_at as created_at_order','orders.delivery_address as delivery_address_order',
+                                    'orders.city as city_order','orders.total_order',
+                                    )
+                            ->get();
 
         }else {
-            $query = 
-                    Order::join('order_details', 'order_details.order_id', 'orders.id')  
+            $query = Order::join('order_details', 'order_details.order_id', 'orders.id')  
                             ->join('plans', 'order_details.plan_id', 'plans.id')   
                             ->join('products', 'plans.product_id', 'products.id')   
                             ->where('orders.user_id', $id)
@@ -69,5 +66,68 @@ class AdminController extends Controller
         }
         $user['transactions'] = $query;
         return  $user;
+    }
+
+    /**
+     * List Orders distribuitors and clients.
+     *
+     * @param role_id $role_id
+     * @return \Illuminate\Http\Response
+     */
+    function listOrdersByRole($role_id)
+    {
+        return User::role($role_id)
+                    ->join('orders', 'orders.user_id', 'users.id')
+                    ->join('order_details', 'order_details.order_id', 'orders.id')  
+                    ->join('status_orders', 'orders.status_id', 'status_orders.id')
+                    ->join('plans', 'order_details.plan_id', 'plans.id')   
+                    ->join('products', 'plans.product_id', 'products.id')  
+                    ->select(
+                            'products.name as name_product', 'products.presentation',
+                            'order_details.quantity', 'users.name as name_user',
+                            'orders.total_order',
+                            'status_orders.status',
+                            )
+                    ->get();
+    }
+
+    /**
+     * Change status to orders.
+     *
+     * @param int $order_id int $status_id
+     * @return \Illuminate\Http\Response
+     */
+    function changeStatusOrder($order_id, $status_id)
+    {
+        try{
+            $order = Order::where('id', $order_id)->update([
+                "status_id" => $status_id
+            ]);
+
+            return response([ 'success' => "Estatus Modificado"]);
+
+        }catch (\Exception $exception){
+            return Response($exception->getMessage(), 500, ['Content-Type' => 'text/plain']);
+        }
+    }
+
+    /**
+     * Change status to products.
+     *
+     * @param int $product_id int $status_id
+     * @return \Illuminate\Http\Response
+     */
+    function changeStatusProduct($product_id, $status_id)
+    {
+        try{
+            $order = Product::where('id', $product_id)->update([
+                "status" => $status_id
+            ]);
+
+            return response([ 'success' => "Estatus Modificado"]);
+
+        }catch (\Exception $exception){
+            return Response($exception->getMessage(), 500, ['Content-Type' => 'text/plain']);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -36,5 +36,8 @@ Route::put('/change-password', 'Api\AuthController@changePassword');
         // Admin routes
         Route::get('/list-distri-client', 'Api\AdminController@listDistriClient');
         Route::get('/detail-distri-client/{id}', 'Api\AdminController@detailDistriClient');
+        Route::get('/list-orders-by-role/{role_id}', 'Api\AdminController@listOrdersByRole');
+        Route::get('/change-status-order/{order_id}/{status_id}', 'Api\AdminController@changeStatusOrder');
+        Route::get('/change-status-product/{product_id}/{status_id}', 'Api\AdminController@changeStatusProduct');
     
     // });
